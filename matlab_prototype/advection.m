f5e488d44aaf4c90438216842e682dd513c859a6
Sam Stewart <sams@umn.edu>
2016-10-04 12:40:12 -0500

    [add] advection code

--- matlab_prototype/advection.m
+++ matlab_prototype/advection.m
@@ -4,15 +4,23 @@
 % substance: a 2D vector array of the substance.
 % dt: change in time
 % dx: size of grid cell
-% 
+% returns
+% advected_substance is the new array after it has been moved by the velocity field.
 function advected_substance = advection(velocity_field, substance, dt, dx) 	
 	N = floor(1/dx);
-	grid_coords = meshgrid(1:N, 1:N);
 
 	% we move backwards in space to see where we came from.
 	% we are really just following characteristics backwards
 	% (looking upstream).
 	old_pos = grid_coords - dt * N * velocity_field;
 	
-	advected_substance = quantity(old_position);
+	% due to the annoyances in matlab's array notation, we cannot do this in one step but
+	% must instead unwind this into a loop.
+
+	for i = 1:N
+		for j = 1:N
+			% we might land "in between" grid cells so we need to interpolate.	
+			advected_substance(i,j) = bilinear_interpolate(substance, old_pos);
+		end
+	end	
 end
